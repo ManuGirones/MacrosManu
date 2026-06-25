e4a3f7424c5436c7c37ef1fe874681429af2c934
ManuGirones <manugironesb@gmail.com>
2026-06-25 15:24:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const C="miplan-v1";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const cc=res.clone();caches.open(C).then(c=>c.put(e.request,cc));return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
